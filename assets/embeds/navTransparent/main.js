c353714764136819f4509342c3482b5ee9622131
TresAbhi <coolabhigyaandeep07@gmail.com>
2021-11-10 00:19:34 +0000

    upgraded to react

--- assets/embeds/navTransparent/main.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-    var searchButton = document.getElementById("search-button");
-    searchButton.children[0].removeAttribute("href");
-    
-    searchButton.onclick = function () {
-        window.parent.showResults();
-    }
-}
