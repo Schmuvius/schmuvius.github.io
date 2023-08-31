38dbd69ad6ac21d0c25057b57c07dfe69c462d47
tresabhi <coolabhigyaandeep07@gmail.com>
2023-08-31 16:26:50 +0000

    feat: a few more items 4

--- src/constants/projects.tsx
+++ src/constants/projects.tsx
@@ -195,7 +195,6 @@ export const projects: Project[] = (
           label: 'Download',
           icon: <DownloadIcon />,
           url: 'https://github.com/bedrockLauncher/BedrockLauncher/releases/latest/download/BedrockLauncherSetup.exe',
-          target: '_blank',
         },
       ],
     },
@@ -212,6 +211,126 @@ export const projects: Project[] = (
           icon: <ArchiveIcon />,
           url: 'https://mcpedl.com/corsa-pack/',
           target: '_blank',
+        },
+      ],
+    },
+
+    {
+      name: 'Music Pause Remover',
+      description: '🎵 Removes the long pauses between music in Minecraft',
+      type: ProjectType.ResourcePack,
+
+      links: [
+        {
+          label: 'MCPEDL',
+          icon: <ArchiveIcon />,
+          url: 'https://mcpedl.com/music-pause-remover-pack/',
+          target: '_blank',
+        },
+        {
+          label: 'Download',
+          icon: <DownloadIcon />,
+          url: 'https://dl.mcpedl.com/texturepacks/5016/Music-Pause-Remover_1613938168.mcpack',
+        },
+      ],
+    },
+
+    {
+      name: 'Achievement Get All',
+      description: '🏆 Easily get all the Achievements in Minecraft',
+      type: ProjectType.ResourcePack,
+
+      links: [
+        {
+          label: 'MCPEDL',
+          icon: <ArchiveIcon />,
+          url: 'https://mcpedl.com/achievement-get-all/',
+          target: '_blank',
+        },
+        {
+          label: 'Download',
+          icon: <DownloadIcon />,
+          url: 'https://dl.mcpedl.com/worlds/10946/Acheivment-Get-All_1610124113.mcworld',
+        },
+      ],
+    },
+
+    {
+      name: 'Flucky',
+      description: '🎲 Randomly rotates many different block textures',
+      type: ProjectType.ResourcePack,
+
+      links: [
+        {
+          label: 'MCPEDL',
+          icon: <ArchiveIcon />,
+          url: 'https://mcpedl.com/flucky-pack/',
+          target: '_blank',
+        },
+        {
+          label: 'Download',
+          icon: <DownloadIcon />,
+          url: 'https://dl.mcpedl.com/texturepacks/3715/Fluky_1604452251.mcpack',
+        },
+      ],
+    },
+
+    {
+      name: 'KLOW',
+      description:
+        '👑 Kinda Like Over World turns the nether into the overworld',
+      type: ProjectType.ResourcePack,
+
+      links: [
+        {
+          label: 'MCPEDL',
+          icon: <ArchiveIcon />,
+          url: 'https://mcpedl.com/klow-pack/',
+          target: '_blank',
+        },
+        {
+          label: 'Download',
+          icon: <DownloadIcon />,
+          url: 'https://dl.mcpedl.com/texturepacks/2678/KLOW_1598303758.mcpack',
+        },
+      ],
+    },
+
+    {
+      name: 'Caramel PVP',
+      description: '⚔️ A streamlined PVP resource pack for Minecraft',
+      type: ProjectType.ResourcePack,
+
+      links: [
+        {
+          label: 'MCPEDL',
+          icon: <ArchiveIcon />,
+          url: 'https://mcpedl.com/pvp-lite/',
+          target: '_blank',
+        },
+        {
+          label: 'Download',
+          icon: <DownloadIcon />,
+          url: 'https://dl.mcpedl.com/texturepacks/2694/Caramel_1606766519.mcpack',
+        },
+      ],
+    },
+
+    {
+      name: 'Procrasti8x8',
+      description: '🔆 An 8x8 recreation of the Minecraft Vanilla textures',
+      type: ProjectType.ResourcePack,
+
+      links: [
+        {
+          label: 'GitHub',
+          icon: <GitHubLogoIcon />,
+          url: 'https://github.com/ProcrastinationNation/Procrasti8x8',
+        },
+        {
+          label: 'Download',
+          icon: <DownloadIcon />,
+          url: 'https://github.com/ProcrastinationNation/Procrasti8x8/archive/refs/heads/master.zip',
         },
       ],
     },
